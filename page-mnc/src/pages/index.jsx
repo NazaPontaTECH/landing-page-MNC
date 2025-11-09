21f7952f9f143268e06f9bc2d1223054a51e06e4
gionogueira <giovannarnogueira@gmail.com>
2025-11-09 02:42:20 -0300

    feat: Inicializar o projeto PageMNC com React e Vite
    - Adicionar package.json com dependências e scripts para desenvolvimento
    - Criar um componente Button
    - Configurar o ponto de entrada principal da aplicação
    - Criar um componente PageMNC que utiliza o Button
    - Adicionar a configuração do Vite para o projeto

---
+++ page-mnc/src/pages/index.jsx
@@ -0,0 +1,12 @@
+import Button from "../components/Button";
+
+function PageMNC() {
+  return (
+    <div>
+      <Button />
+      <div>Page MNC</div>
+    </div>
+  );
+}
+
+export default PageMNC;
